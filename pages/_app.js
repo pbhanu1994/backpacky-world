88dfa9449997110ac66d4ae5f468dcb24d445a6c
Bhanu <pbhanu.1994@gmail.com>
2021-04-17 16:00:59 +1000

    Added the _document file and fixed the material-ui flckering in SSR

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,40 @@
-import withRedux from 'next-redux-wrapper';
-import initStore from '../redux/store';
+import React, { useEffect } from 'react';
 import App from 'next/app';
+import Head from 'next/head';
+import withRedux from 'next-redux-wrapper';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import initStore from '../redux/store';
+import theme from '../src/theme';
 import { AuthProvider } from '../src/auth';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps, store, ...rest }) {
+  
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  
   return (
-    // <Provider store={initStore}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    // </Provider>
+    <React.Fragment>
+      <Head>
+          <title>My page</title>
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        </Head>
+      {/* // <Provider store={initStore}> */}
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </ThemeProvider>
+      {/* // </Provider> */}
+    </React.Fragment>
   )
 }
 
