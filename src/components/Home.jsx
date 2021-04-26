d417a6dcb52aeffa31fc8caad1997c6f3b25604e
Bhanu <pbhanu.1994@gmail.com>
2021-04-26 17:55:45 +1000

    Added small welcome message for the user on home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,17 +16,36 @@ import {
   Typography,
   Container,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { auth } from "../firebaseClient";
 import Sidebar from "./Sidebar";
 
+const useStyles = makeStyles((theme) => ({
+  welcomeText: {
+    color: theme.palette.primary.main,
+  },
+}));
+
 export default function Home({ userId }) {
   const router = useRouter();
+  const classes = useStyles();
+
   return (
     <Grid container spacing={4}>
       <Grid item xs={2} md={3}>
         <Sidebar />
       </Grid>
       <Grid item xs={10} md={9}>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          Welcome, <span className={classes.welcomeText}>Bhanu!</span>
+        </Typography>
+
         <h1>Welcome to Dashboard</h1>
         <h3>Have a Safe Travel!</h3>
         <h2>UID: {userId}</h2>
