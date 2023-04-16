39909e34c72600a3c0fffa1751b390c1128fba8a
Bhanu Prakash <pbhanu.1994@gmail.com>
2023-04-17 03:40:31 +1000

    removed unwanted package

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers/rootReducer";
 
 const persistConfig = {
@@ -16,7 +15,6 @@ const store = configureStore({
   reducer: persistedReducer,
   middleware: [thunk],
   devTools: process.env.NODE_ENV !== "production",
-  composeWithDevTools,
 });
 
 export const persistor = persistStore(store);
