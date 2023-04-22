6ee828fd2d788588553748405f7163d94113c66c
Bhanu Prakash <pbhanu.1994@gmail.com>
2023-04-22 15:55:01 +1000

    Added menu popover for the mobile view for the To-do list

--- src/components/templates/Journal/ToDoList/Option.jsx
+++ src/components/templates/Journal/ToDoList/Option.jsx
@@ -6,14 +6,14 @@ import {
   Checkbox,
   Divider,
   IconButton,
+  MenuItem,
+  Stack,
+  useMediaQuery,
 } from "@mui/material";
-import {
-  Edit as EditIcon,
-  EditOutlined as EditOutlinedIcon,
-  Delete as DeleteIcon,
-  DeleteOutline as DeleteOutlineIcon,
-} from "@mui/icons-material";
 import { EditSectionText } from "/src/components/atoms/EditSectionText";
+import Iconify from "/src/components/atoms/Iconify";
+import { MenuPopover } from "/src/components/atoms/MenuPopover";
+import { useTheme } from "@mui/system";
 import { listStyles, CustomListItemText } from "./listStyles";
 
 export const Option = ({
@@ -23,15 +23,30 @@ export const Option = ({
   onUpdateitem,
   onDeleteItem,
 }) => {
-  const [mouseHover, setMouseHover] = useState(false);
   const [mouseHoverOnItem, setMouseHoverOnItem] = useState(false);
-  const [mouseHoverOnEdit, setMouseHoverOnEdit] = useState(false);
   const [showitemInput, setShowitemInput] = useState(false);
   const [itemId, setitemId] = useState(null);
+  const [open, setOpen] = useState(null);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
 
   const labelId = `checkbox-list-label-${item.name}`;
   const checkboxChecked = checked !== false;
   const classes = listStyles({ checkboxChecked });
+
+  const handleOpen = (event) => {
+    setOpen(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const ICON = {
+    mr: 2,
+    width: 20,
+    height: 20,
+  };
 
   return (
     <>
@@ -86,38 +101,99 @@ export const Option = ({
             />
           )}
         </Grid>
-        <span
-          style={{
-            display: "flex",
-            visibility: mouseHoverOnItem ? "visible" : "hidden", //TODO: Check this with experienced dev, if it's a good approach
-          }}
-        >
-          <IconButton
-            aria-label="Edit Item Name"
-            onClick={() => {
-              setShowitemInput(true);
-              setitemId(item.id);
+        {!isMobile && (
+          <span
+            style={{
+              display: "flex",
+              visibility: mouseHoverOnItem ? "visible" : "hidden", //TODO: Check this with experienced dev, if it's a good approach
             }}
-            onMouseOver={() => setMouseHoverOnEdit(true)}
-            onMouseLeave={() => setMouseHoverOnEdit(false)}
           >
-            {/* TODO: Check the colors of Edit - Secondary or black, etc. on hover with a designer */}
-            {mouseHoverOnEdit ? (
-              <EditIcon color="secondary" />
-            ) : (
-              <EditOutlinedIcon />
-            )}
-          </IconButton>
-          <IconButton
-            aria-label="Delete Item"
-            onClick={() => onDeleteItem(sectionId, item)}
-            onMouseOver={() => setMouseHover(true)}
-            onMouseLeave={() => setMouseHover(false)}
-          >
-            {/* TODO: Check the colors of error or black on hover with a designer */}
-            {mouseHover ? <DeleteIcon color="error" /> : <DeleteOutlineIcon />}
-          </IconButton>
-        </span>
+            <IconButton
+              aria-label="Edit Item Name"
+              color="secondary"
+              size="medium"
+              onClick={() => {
+                setShowitemInput(true);
+                setitemId(item.id);
+              }}
+            >
+              {/* TODO: Check the colors of Edit - Secondary or black, etc. on hover with a designer */}
+              <Iconify icon={"eva:edit-fill"} />
+            </IconButton>
+            <IconButton
+              aria-label="Delete Item"
+              sx={classes.deleteSectionButton}
+              size="medium"
+              onClick={() => onDeleteItem(sectionId, item)}
+            >
+              {/* TODO: Check the colors of error or black on hover with a designer */}
+              <Iconify icon={"eva:trash-2-outline"} />
+            </IconButton>
+          </span>
+        )}
+        {isMobile && (
+          <span>
+            <IconButton size="large" onClick={handleOpen}>
+              <Iconify icon={"eva:more-vertical-fill"} width={20} height={20} />
+            </IconButton>
+
+            <MenuPopover
+              open={Boolean(open)}
+              anchorEl={open}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: "top", horizontal: "left" }}
+              transformOrigin={{ vertical: "top", horizontal: "right" }}
+              arrow="right-top"
+              sx={{
+                mt: -0.5,
+                width: "auto",
+                "& .MuiMenuItem-root": {
+                  p: 1,
+                  typography: "body2",
+                  borderRadius: 0.75,
+                },
+              }}
+            >
+              <Stack sx={{ p: 1 }}>
+                <MenuItem
+                  onClick={() => {
+                    if (!showitemInput) {
+                      onUpdateitem(sectionId, item, true, null);
+                      handleClose();
+                    }
+                  }}
+                >
+                  <Iconify
+                    icon={"eva:checkmark-circle-2-fill"}
+                    sx={{ ...ICON }}
+                  />
+                  {checked ? "Mark Incomplete" : "Mark Complete"}
+                </MenuItem>
+
+                <MenuItem
+                  onClick={() => {
+                    setShowitemInput(true);
+                    setitemId(item.id);
+                    handleClose();
+                  }}
+                >
+                  <Iconify icon={"eva:edit-fill"} sx={{ ...ICON }} />
+                  Edit
+                </MenuItem>
+
+                <Divider sx={{ my: 1 }} />
+
+                <MenuItem
+                  sx={{ color: "error.main" }}
+                  onClick={() => onDeleteItem(sectionId, item)}
+                >
+                  <Iconify icon={"eva:trash-2-outline"} sx={{ ...ICON }} />
+                  Delete
+                </MenuItem>
+              </Stack>
+            </MenuPopover>
+          </span>
+        )}
       </ListItem>
       <Divider light />
     </>
--- src/components/templates/Journal/ToDoList/index.jsx
+++ src/components/templates/Journal/ToDoList/index.jsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Divider, List, Paper, Grid, IconButton } from "@mui/material";
 import {
-  EditOutlined as EditIcon,
-  DeleteOutline as DeleteOutlineIcon,
-} from "@mui/icons-material";
-import { Container, Typography } from "@mui/material";
+  Container,
+  Typography,
+  MenuItem,
+  Stack,
+  useMediaQuery,
+} from "@mui/material";
 // import { GiLightBackpack } from "react-icons/gi";
 import Page from "../../../atoms/Page";
 import DashboardLayout from "../../../layouts/dashboard";
@@ -16,6 +18,9 @@ import { AddSection } from "./AddSection";
 import { PAGE_PATH } from "../../../../constants/navigationConstants";
 import HeaderBreadcrumbs from "../../../atoms/HeaderBreadCrumbs";
 import setAndShowDeleteDialog from "../../../../store/actions/config/dialog/setAndShowDeleteDialog";
+import Iconify from "/src/components/atoms/Iconify";
+import { MenuPopover } from "/src/components/atoms/MenuPopover";
+import { useTheme } from "@mui/system";
 import { listStyles } from "./listStyles";
 
 export default function ToDoList({
@@ -33,6 +38,23 @@ export default function ToDoList({
   const [mouseHoverOnSection, setMouseHoverOnSection] = useState(false);
   const [editSectionId, setEditSectionId] = useState(null);
   const [showSectionUpdateInput, setShowSectionUpdateInput] = useState(false);
+  const [open, setOpen] = useState(null);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
+
+  const handleOpen = (e, id) => {
+    setOpen({ anchorEl: e.currentTarget, id });
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const ICON = {
+    mr: 2,
+    width: 20,
+    height: 20,
+  };
 
   const dispatch = useDispatch();
   const { themeStretch } = useSettings();
@@ -69,7 +91,11 @@ export default function ToDoList({
                   <Grid
                     container
                     justifyContent="space-between"
-                    style={{ padding: "0.8rem 1rem", height: "3.7rem" }}
+                    style={{
+                      padding: "0.8rem 1rem",
+                      height: "3.7rem",
+                      flexWrap: "nowrap",
+                    }}
                     onMouseOver={() => {
                       setSelectedHoverSectionId(item.sectionId);
                       setMouseHoverOnSection(true);
@@ -95,42 +121,43 @@ export default function ToDoList({
                         }}
                       >
                         {item.sectionTitle} &nbsp;
-                        {selectedHoverSectionId === item.sectionId && (
-                          <span
-                            style={{
-                              display: "flex",
-                            }}
-                          >
-                            {/* TODO: Check the colors of Edit & Delete Icons (outlined or filled etc...) */}
-                            <IconButton
-                              aria-label="Edit section name"
-                              size="small"
-                              color="secondary"
-                              onClick={() => {
-                                setShowSectionUpdateInput(true);
-                                setEditSectionId(item.sectionId);
+                        {!isMobile &&
+                          selectedHoverSectionId === item.sectionId && (
+                            <span
+                              style={{
+                                display: "flex",
                               }}
                             >
-                              <EditIcon />
-                            </IconButton>{" "}
-                            &nbsp;
-                            <IconButton
-                              aria-label="Delete section"
-                              size="small"
-                              sx={classes.deleteSectionButton}
-                              onClick={() => {
-                                dispatch(
-                                  setAndShowDeleteDialog(
-                                    item.sectionTitle,
-                                    () => onDeleteSection(item.sectionId) // Storing function reference (callback?) in the store to use later
-                                  )
-                                );
-                              }}
-                            >
-                              <DeleteOutlineIcon color="error" />
-                            </IconButton>
-                          </span>
-                        )}
+                              {/* TODO: Check the colors of Edit & Delete Icons (outlined or filled etc...) */}
+                              <IconButton
+                                aria-label="Edit section name"
+                                size="medium"
+                                color="secondary"
+                                onClick={() => {
+                                  setShowSectionUpdateInput(true);
+                                  setEditSectionId(item.sectionId);
+                                }}
+                              >
+                                <Iconify icon={"eva:edit-fill"} />
+                              </IconButton>{" "}
+                              &nbsp;
+                              <IconButton
+                                aria-label="Delete section"
+                                size="medium"
+                                sx={classes.deleteSectionButton}
+                                onClick={() => {
+                                  dispatch(
+                                    setAndShowDeleteDialog(
+                                      item.sectionTitle,
+                                      () => onDeleteSection(item.sectionId) // Storing function reference (callback?) in the store to use later
+                                    )
+                                  );
+                                }}
+                              >
+                                <Iconify icon={"eva:trash-2-outline"} />
+                              </IconButton>
+                            </span>
+                          )}
                       </Typography>
                     )}
                     {showSectionUpdateInput &&
@@ -147,23 +174,107 @@ export default function ToDoList({
                         />
                       )}
                     {/* TODO: Check with the designer if hiding the completed items on hover is a good idea */}
-                    {(!mouseHoverOnSection ||
-                      selectedHoverSectionId !== item.sectionId) && (
-                      <Typography
-                        component="h6"
-                        variant="body1"
-                        style={{
-                          display: "flex",
-                          alignItems: "center",
-                        }}
-                      >
-                        Completed:{" "}
-                        {
-                          item.sectionItems?.filter((item) => item.checked)
-                            .length
-                        }{" "}
-                        / {item.sectionItems?.length}
-                      </Typography>
+                    {!isMobile &&
+                      (!mouseHoverOnSection ||
+                        selectedHoverSectionId !== item.sectionId) && (
+                        <Typography
+                          component="h6"
+                          variant="body1"
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                          }}
+                        >
+                          Completed:{" "}
+                          {
+                            item.sectionItems?.filter((item) => item.checked)
+                              .length
+                          }{" "}
+                          / {item.sectionItems?.length}
+                        </Typography>
+                      )}
+                    {isMobile && (
+                      <span>
+                        <IconButton
+                          size="large"
+                          onClick={(event) => handleOpen(event, item.sectionId)}
+                        >
+                          <Iconify
+                            icon={"eva:more-vertical-fill"}
+                            width={20}
+                            height={20}
+                          />
+                        </IconButton>
+                        <MenuPopover
+                          id={`menu-popover-${item.sectionId}`}
+                          open={open?.id === item.sectionId}
+                          anchorEl={open?.anchorEl}
+                          onClose={handleClose}
+                          anchorOrigin={{
+                            vertical: "top",
+                            horizontal: "left",
+                          }}
+                          transformOrigin={{
+                            vertical: "top",
+                            horizontal: "right",
+                          }}
+                          arrow="right-top"
+                          sx={{
+                            mt: -0.5,
+                            width: "auto",
+                            "& .MuiMenuItem-root": {
+                              p: 1,
+                              typography: "body2",
+                              borderRadius: 0.75,
+                            },
+                          }}
+                        >
+                          <Stack sx={{ p: 1 }}>
+                            <Typography
+                              variant="body2"
+                              sx={{ color: "text.secondary", py: 0.5 }}
+                              noWrap
+                            >
+                              {item.sectionTitle}
+                            </Typography>
+
+                            <MenuItem
+                              onClick={() => {
+                                setShowSectionUpdateInput(true);
+                                setEditSectionId(item.sectionId);
+                                handleClose();
+                              }}
+                            >
+                              <Iconify
+                                icon={"eva:edit-fill"}
+                                sx={{ ...ICON }}
+                              />
+                              Edit
+                            </MenuItem>
+
+                            <Divider sx={{ my: 1 }} />
+
+                            <MenuItem
+                              sx={{ color: "error.main" }}
+                              onClick={() => {
+                                dispatch(
+                                  setAndShowDeleteDialog(
+                                    item.sectionTitle,
+                                    () => onDeleteSection(item.sectionId) // Storing function reference (callback?) in the store to use later
+                                  )
+                                );
+                                handleClose();
+                              }}
+                            >
+                              <Iconify
+                                icon={"eva:trash-2-outline"}
+                                sx={{ ...ICON }}
+                              />
+                              Delete
+                            </MenuItem>
+                          </Stack>
+                        </MenuPopover>
+                      </span>
                     )}
                     {/* <h3 style={{ fontWeight: 400 }}>Total: 31 Items</h3> */}
                   </Grid>
