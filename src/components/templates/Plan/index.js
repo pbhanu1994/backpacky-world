9bb7cdac0557f92eccc1a6292880591beac2e942
Bhanu Prakash <pbhanu.1994@gmail.com>
2023-06-04 22:05:40 +1000

    Implemented the PlanTrip component with sub components

--- src/components/templates/Plan/index.js
+++ src/components/templates/Plan/index.js
@@ -10,23 +10,24 @@ import {
   CircularProgress,
 } from "@mui/material";
 import dayjs from "dayjs";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { useFormik, Form, FormikProvider } from "formik";
+import axios from "axios";
 import * as Yup from "yup";
+import { useFormik, Form, FormikProvider } from "formik";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import DashboardLayout from "../../layouts/dashboard";
 import Page from "../../atoms/Page";
 import useSettings from "../../../hooks/useSettings";
-import axios from "axios";
+import PlanTrip from "./PlanTrip";
 
 export default function Plan({ userId }) {
-  const [inputValue, setInputValue] = useState("");
   const [loading, setLoading] = useState(false);
   const [options, setOptions] = useState([]);
+  const [showPlanTrip, setShowPlanTrip] = useState(false);
 
   const { themeStretch } = useSettings();
 
   // Validation Schema
-  const signUpSchema = Yup.object().shape({
+  const planTripSchema = Yup.object().shape({
     location: Yup.string().required(),
   });
 
@@ -35,25 +36,19 @@ export default function Plan({ userId }) {
       location: "",
       date: dayjs(new Date()), //TODO: get the default value (date) => dayjs(new Date(date) || new Date())
     },
-    validationSchema: signUpSchema,
+    validationSchema: planTripSchema,
     onSubmit: async (values) => {
-      const { location, date } = values;
-      //   console.log("location", location);
-      //   console.log("date", date);
+      // const { location, date } = values;
 
       if (_.isEmpty(errors)) {
-        // setShowLoadingButton(true);
-        // const result = await dispatch(
-        //   signUpUser(email, password, firstName, lastName)
-        // );
-        // result === "error" && setShowLoadingButton(false);
+        setShowPlanTrip(true);
       }
     },
   });
 
   const handleInputChange = (event, value) => {
     setLoading(true);
-    setInputValue(value);
+    setFieldValue("location", value);
 
     // Make a request to the Google Places Autocomplete API
     axios
@@ -82,133 +77,141 @@ export default function Plan({ userId }) {
       });
   };
 
-  const { errors, touched, setFieldValue, handleSubmit, getFieldProps } =
-    formik;
+  const { errors, touched, values, setFieldValue, handleSubmit } = formik;
+
+  const { location, date } = values;
 
   return (
     <DashboardLayout>
-      <Page title="Plan | BackpackyWorld">
-        <Container maxWidth={themeStretch ? false : "xl"}>
-          <Box sx={{ flexGrow: 1 }}>
-            <Box
-              sx={{
-                position: "relative",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "100%",
-                mt: "14%",
-              }}
-            >
-              <Container maxWidth="md">
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    mb: 4,
-                  }}
-                >
-                  <Typography
-                    variant="h2"
-                    component="h1"
-                    align="center"
-                    gutterBottom
+      {!showPlanTrip && (
+        <Page title="Plan | BackpackyWorld">
+          <Container maxWidth={themeStretch ? false : "xl"}>
+            <Box sx={{ flexGrow: 1 }}>
+              <Box
+                sx={{
+                  position: "relative",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "100%",
+                  mt: "14%",
+                }}
+              >
+                <Container maxWidth="md">
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      mb: 4,
+                    }}
                   >
-                    Plan your next adventure
-                  </Typography>
-                </Box>
-                <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
-                  <FormikProvider value={formik}>
-                    <Form
-                      style={{ width: "100%", marginTop: 24 }}
-                      autoComplete="off"
-                      noValidate
-                      onSubmit={handleSubmit}
+                    <Typography
+                      variant="h2"
+                      component="h1"
+                      align="center"
+                      gutterBottom
                     >
-                      <Grid container spacing={2} sx={{ alignItems: "center" }}>
-                        <Grid item xs={12} md={6}>
-                          {/* TODO: Have a re-usable component of countries dropdown */}
-                          <Autocomplete
-                            freeSolo
-                            disableClearable
-                            options={options.map((option) => ({
-                              label: option.name,
-                            }))}
-                            inputValue={inputValue}
-                            onInputChange={handleInputChange}
-                            renderInput={(params) => (
-                              <TextField
-                                {...params}
-                                fullWidth
-                                label="Where do you want to go?"
-                                variant="outlined"
-                                color="primary"
-                                autoFocus
-                                required
-                                {...getFieldProps("location")}
-                                error={Boolean(
-                                  touched.location && errors.location
-                                )}
-                                InputProps={{
-                                  ...params.InputProps,
-                                  endAdornment: (
-                                    <React.Fragment>
-                                      {loading ? (
-                                        <CircularProgress
-                                          color="inherit"
-                                          size={20}
-                                        />
-                                      ) : null}
-                                      {params.InputProps.endAdornment}
-                                    </React.Fragment>
-                                  ),
-                                }}
-                              />
-                            )}
-                            renderOption={(props, option) => {
-                              return (
-                                <li {...props}>
-                                  <Box display="flex" alignItems="center">
-                                    <Typography>{option.label}</Typography>
-                                  </Box>
-                                </li>
-                              );
-                            }}
-                          />
+                      Plan your next adventure
+                    </Typography>
+                  </Box>
+                  <Box
+                    sx={{ display: "flex", justifyContent: "center", mt: 4 }}
+                  >
+                    <FormikProvider value={formik}>
+                      <Form
+                        style={{ width: "100%", marginTop: 24 }}
+                        autoComplete="off"
+                        noValidate
+                        onSubmit={handleSubmit}
+                      >
+                        <Grid
+                          container
+                          spacing={2}
+                          sx={{ alignItems: "center" }}
+                        >
+                          <Grid item xs={12} md={6}>
+                            {/* TODO: Have a re-usable component of countries dropdown */}
+                            <Autocomplete
+                              freeSolo
+                              disableClearable
+                              options={options.map((option) => ({
+                                label: option.name,
+                              }))}
+                              inputValue={location}
+                              onInputChange={handleInputChange}
+                              renderInput={(params) => (
+                                <TextField
+                                  {...params}
+                                  fullWidth
+                                  label="Where do you want to go?"
+                                  variant="outlined"
+                                  color="primary"
+                                  autoFocus
+                                  required
+                                  error={Boolean(
+                                    touched.location && errors.location
+                                  )}
+                                  InputProps={{
+                                    ...params.InputProps,
+                                    endAdornment: (
+                                      <React.Fragment>
+                                        {loading ? (
+                                          <CircularProgress
+                                            color="inherit"
+                                            size={20}
+                                          />
+                                        ) : null}
+                                        {params.InputProps.endAdornment}
+                                      </React.Fragment>
+                                    ),
+                                  }}
+                                />
+                              )}
+                              renderOption={(props, option) => {
+                                return (
+                                  <li {...props}>
+                                    <Box display="flex" alignItems="center">
+                                      <Typography>{option.label}</Typography>
+                                    </Box>
+                                  </li>
+                                );
+                              }}
+                            />
+                          </Grid>
+                          <Grid item xs={12} md={4}>
+                            <DatePicker
+                              label="When?"
+                              onChange={(dateValue) =>
+                                setFieldValue("date", dateValue)
+                              }
+                              value={date}
+                              format="DD/MM/YYYY"
+                              sx={{ width: "100%" }}
+                            />
+                          </Grid>
+                          <Grid item xs={12} md={2}>
+                            <Button
+                              type="submit"
+                              fullWidth
+                              variant="contained"
+                              color="primary"
+                              size="large"
+                            >
+                              Search
+                            </Button>
+                          </Grid>
                         </Grid>
-                        <Grid item xs={12} md={4}>
-                          <DatePicker
-                            label="When?"
-                            {...getFieldProps("date")}
-                            onChange={(dateValue) =>
-                              setFieldValue("date", dateValue)
-                            }
-                            format="DD/MM/YYYY"
-                            sx={{ width: "100%" }}
-                            // onChange={(dateValue) => setDate(dateValue)}
-                          />
-                        </Grid>
-                        <Grid item xs={12} md={2}>
-                          <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            size="large"
-                          >
-                            Search
-                          </Button>
-                        </Grid>
-                      </Grid>
-                    </Form>
-                  </FormikProvider>
-                </Box>
-              </Container>
+                      </Form>
+                    </FormikProvider>
+                  </Box>
+                </Container>
+              </Box>
             </Box>
-          </Box>
-        </Container>
-      </Page>
+          </Container>
+        </Page>
+      )}
+      {showPlanTrip && <PlanTrip location={location} date={date} />}
     </DashboardLayout>
   );
 }
