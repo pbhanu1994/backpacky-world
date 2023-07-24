1c8109c11443efbf18eb6c8c446365794c4fffa0
Bhanu Prakash <pbhanu.1994@gmail.com>
2023-07-24 23:48:15 +1000

    Implemented Hotels list by geoCode API

---
+++ src/services/hotel/hotelsByGeoCode.js
@@ -0,0 +1,31 @@
+import { setAuthorizationHeader, get } from "../../helpers/http";
+import { getAccessToken } from "../auth/amadeusAuth";
+import setAndShowErrorToast from "../../store/actions/config/toast/setAndShowErrorToast";
+
+// Function to perform a hotel search
+export const performHotelSearchByGeoCode = async (
+  dispatch,
+  latitude,
+  longitude,
+  radius = 5
+) => {
+  const accessToken = await getAccessToken();
+
+  const apiEndpoint =
+    "https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-geocode";
+  const params = {
+    latitude,
+    longitude,
+    radius,
+  };
+
+  setAuthorizationHeader(accessToken);
+  try {
+    const hotelSearchData = await get(apiEndpoint, params);
+    return hotelSearchData;
+  } catch (err) {
+    console.error("Error occurred:", err.message);
+    dispatch(setAndShowErrorToast(err.message));
+    return "error";
+  }
+};
