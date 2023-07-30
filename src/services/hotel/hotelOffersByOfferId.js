6f6f1654d886b686ce78ef20603cf0af820af1b9
Bhanu Prakash <pbhanu.1994@gmail.com>
2023-07-30 14:42:52 +1000

    Implemented the getHotelOffersByOfferId API endpoint

--- src/services/hotel/hotelOffersByOfferId.js
+++ src/services/hotel/hotelOffersByOfferId.js
@@ -2,30 +2,24 @@ import { setAuthorizationHeader, get } from "../../helpers/http";
 import { getAccessToken } from "../auth/amadeusAuth";
 import setAndShowErrorToast from "../../store/actions/config/toast/setAndShowErrorToast";
 
-// Function to get a hotel offers by Hotel Ids
-export const getHotelOffersByHotelIds = async (
+// Function to get a hotel offers by Offer Id
+export const getHotelOffersByOfferId = async (
   dispatch,
-  hotelIds,
-  adults = 1,
-  checkInDate,
-  checkOutDate,
-  roomQuantity = 1
+  offerId,
+  language = null // Language - FR , fr , fr-FR., if language is not available the text will be returned in english.
 ) => {
   const accessToken = await getAccessToken();
 
   const apiEndpoint = "https://test.api.amadeus.com/v3/shopping/hotel-offers";
   const params = {
-    hotelIds, // List of HotelIds ["XXXXXXX", "XXXXXXX"]
-    adults,
-    checkInDate, // Example : 2023-11-22
-    checkOutDate, // Example : 2023-12-18
-    roomQuantity, // Number of rooms requested (1-9).
+    offerId,
+    lang: language,
   };
 
   setAuthorizationHeader(accessToken);
   try {
-    const hotelOffersData = await get(apiEndpoint, params);
-    return hotelOffersData;
+    const hotelOfferData = await get(apiEndpoint, params);
+    return hotelOfferData;
   } catch (err) {
     console.error("Error occurred:", err.message);
     dispatch(setAndShowErrorToast(err.message));
